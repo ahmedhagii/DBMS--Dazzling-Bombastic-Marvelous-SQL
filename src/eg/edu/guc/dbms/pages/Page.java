48e168915bfe89449b3a61ce42fc365d38b95b8d
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-06-05 13:41:49 +0200

    ta3deel el pages

--- src/eg/edu/guc/dbms/pages/Page.java
+++ src/eg/edu/guc/dbms/pages/Page.java
@@ -1,21 +1,22 @@
 package eg.edu.guc.dbms.pages;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Page {
 	
 	String tableName;
-	ArrayList<String> tuples = new ArrayList<String>();
+	ArrayList<Hashtable<String,String>> tuples = new ArrayList<Hashtable<String,String>>();
 	
 	public Page() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<String> getTuples() {
+	public ArrayList<Hashtable<String,String>> getTuples() {
 		return tuples;
 	}
 
-	public void setTuples(ArrayList<String> tuples) {
+	public void setTuples(ArrayList<Hashtable<String,String>> tuples) {
 		this.tuples = tuples;
 	}
 
