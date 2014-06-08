c49a4825c9585cafaf5fc753399be34b4d0780b3
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-06-08 09:10:03 +0200

    eh ba2a eh?

--- src/eg/edu/guc/dbms/steps/PageWrite.java
+++ src/eg/edu/guc/dbms/steps/PageWrite.java
@@ -1,15 +1,36 @@
 package eg.edu.guc.dbms.steps;
 
+import java.io.IOException;
+
+import eg.edu.guc.dbms.classes.BufferManager;
+import eg.edu.guc.dbms.pages.Page;
+import eg.edu.guc.dbms.pages.PageID;
+
 public class PageWrite extends Step {
 
 	public PageWrite() {
 		// TODO Auto-generated constructor stub
 	}
+ 	private BufferManager bm ;
+ 	private PageID pageID;
+ 	private Page page;
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
 		
+		try {
+			bm.write(pageID, page);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public PageWrite(BufferManager bm, PageID pageID, Page page) {
+		super();
+		this.bm = bm;
+		this.pageID = pageID;
+		this.page = page;
 	}
 	
 }
